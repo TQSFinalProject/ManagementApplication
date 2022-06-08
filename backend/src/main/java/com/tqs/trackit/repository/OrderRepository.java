57059655b7f3c26b2bb841556d75daa58dfaae60
Miguel Ferreira <miguel.r.ferreira@ua.pt>
2022-06-08 14:40:57 +0100

    findByRiderId added to repository and tests added

--- backend/src/main/java/com/tqs/trackit/repository/OrderRepository.java
+++ backend/src/main/java/com/tqs/trackit/repository/OrderRepository.java
@@ -2,10 +2,12 @@ package com.tqs.trackit.repository;
 
 import com.tqs.trackit.model.Order;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long>{
-    
+    public List<Order> findByRiderId(Long riderId);
 }
--- backend/src/test/java/com/tqs/trackit/repository/OrderRepositoryTest.java
+++ backend/src/test/java/com/tqs/trackit/repository/OrderRepositoryTest.java
@@ -11,6 +11,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.List;
+import java.util.Optional;
 
 @DataJpaTest
 public class OrderRepositoryTest {
@@ -34,5 +36,25 @@ public class OrderRepositoryTest {
     void whenInvalidId_thenReturnNull() {
         Order fromDb = orderRepository.findById(-111L).orElse(null);
         assertThat(fromDb).isNull();
+    }
+
+    @Test
+    void whenFindOrderByExistingRiderId_thenReturnListOfOrders() {
+        Order order1 = new Order("Late","Home Y", LocalDateTime.of(2022,Month.JANUARY,7,19,43,20),LocalDateTime.of(2022,Month.JANUARY,7,19,20,10),LocalDateTime.of(2022,Month.JANUARY,7,19,45,32),1L,1L,"Wine X","9183725364",4.5);
+        Order order2 = new Order("On Time", "Home X", LocalDateTime.of(2022, Month.JANUARY, 7, 15, 43, 00), LocalDateTime.of(2022, Month.JANUARY, 7, 15, 30, 10), LocalDateTime.of(2022, Month.JANUARY, 7, 15, 35, 10), 1L, 1L, "Wine Y", "9183725354", 4.0);
+
+        entityManager.persistAndFlush(order1);
+        entityManager.persistAndFlush(order2);
+
+        List<Order> fromDb = orderRepository.findByRiderId(1L);
+        assertThat(fromDb.size()).isEqualTo(2);
+        assertThat(fromDb.get(0).getOrderDetails()).isEqualTo("Wine X");
+        assertThat(fromDb.get(1).getOrderDetails()).isEqualTo("Wine Y");
+    }
+
+    @Test
+    void whenInvalidRiderId_thenReturnEmptyList() {
+        List<Order> fromDb = orderRepository.findByRiderId(-111L);
+        assertThat(fromDb.size()).isEqualTo(0);
     }
 }
